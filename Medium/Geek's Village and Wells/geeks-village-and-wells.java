daee218a9b3b0050b6b8c21e62ba8cce2eb4a37c
RAM ANUJ PATHAK <109123656+PathakAnuj@users.noreply.github.com>
2023-04-28 00:39:01 +0530

    Updated Solution - GfG to GitHub

--- Medium/Geek's Village and Wells/geeks-village-and-wells.java
+++ Medium/Geek's Village and Wells/geeks-village-and-wells.java
@@ -44,14 +44,14 @@ class Solution
             
     public int[][] chefAndWells(int m,int n,char arr[][])
     {
-        int ram[][] = new int[m][n];
-        for(int r[] : ram) Arrays.fill(r, Integer.MAX_VALUE);
+        int res[][] = new int[m][n];
+        for(int r[] : res) Arrays.fill(r, Integer.MAX_VALUE);
         
         Queue<int[]> q = new LinkedList<>();
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
                 if(arr[i][j] == 'W'){
-                    ram[i][j] = 0;
+                    res[i][j] = 0;
                     q.add(new int[]{i, j, 0});
                 }
             }
@@ -61,8 +61,8 @@ class Solution
             for(int i = 0;i<4;i++){
                 int r = curr[0]+dir[i][0],  c = curr[1]+dir[i][1]; 
                 if(r>=0 && c>=0 && r<m && c<n && (arr[r][c] == 'H' || arr[r][c] == '.')){
-                    if(ram[r][c]>curr[2]+2){
-                        ram[r][c] = curr[2]+2;
+                    if(res[r][c]>curr[2]+2){
+                        res[r][c] = curr[2]+2;
                         q.add(new int[]{r, c, curr[2]+2});
                     }
                 }
@@ -71,13 +71,13 @@ class Solution
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
                 
-                if(ram[i][j] == Integer.MAX_VALUE) ram[i][j] = -1;
+                if(res[i][j] == Integer.MAX_VALUE) res[i][j] = -1;
                 if(arr[i][j] == '.' || arr[i][j] == 'N'){
-                    ram[i][j] = 0;
+                    res[i][j] = 0;
                 }
             }
         }
-        return ram;
+        return res;
         
     }
 }
