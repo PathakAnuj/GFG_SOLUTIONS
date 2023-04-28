13477494732db5fec70c8a9b9d17de5a5a30add2
RAM ANUJ PATHAK <109123656+PathakAnuj@users.noreply.github.com>
2023-04-29 00:13:02 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find Number/find-number.java
+++ Medium/Find Number/find-number.java
@@ -24,18 +24,14 @@ class GFG {
 
 
 
-
-
-//User function Template for Java
-//User function Template for Java
 class Solution
 {
     public long findNumber(long N)
     {
 		long[] v = { 9, 1, 3, 5, 7 };
-	    long anuj = 0;
+	    long vikrantKiRahnuma = 0;
 	    while (N > 0) {
-	      anuj = anuj * 10 + v[(int)(N % 5)];
+	      vikrantKiRahnuma = vikrantKiRahnuma * 10 + v[(int)(N % 5)];
 	      if (N % 5 == 0) {
 	        N = N / 5;
 	        N--;
@@ -44,14 +40,14 @@ class Solution
 	        N /= 5;
 	    }
 	    Queue<Long> q = new ArrayDeque<>();
-	    while (anuj > 0) {
-	      q.add(anuj % 10);
-	      anuj /= 10;
+	    while (vikrantKiRahnuma > 0) {
+	      q.add(vikrantKiRahnuma % 10);
+	      vikrantKiRahnuma /= 10;
 	    }
 	    while (!q.isEmpty()) {
-	      anuj = anuj * 10 + q.peek();
+	      vikrantKiRahnuma = vikrantKiRahnuma * 10 + q.peek();
 	      q.remove();
 	    }
-	    return anuj;
+	    return vikrantKiRahnuma;
     }
 }
