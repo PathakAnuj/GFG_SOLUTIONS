499885f2b95d451e8f706a27ab24f96c2fe1d047
RAM ANUJ PATHAK <109123656+PathakAnuj@users.noreply.github.com>
2023-05-06 00:08:28 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Another Coin Change Problem/another-coin-change-problem.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int N;
+            N = Integer.parseInt(br.readLine());
+
+            int K;
+            K = Integer.parseInt(br.readLine());
+
+            int target;
+            target = Integer.parseInt(br.readLine());
+
+            int[] coins = IntArray.input(br, N);
+
+            Solution obj = new Solution();
+            boolean res = obj.makeChanges(N, K, target, coins);
+
+            int _result_val = (res) ? 1 : 0;
+            System.out.println(_result_val);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static boolean makeChanges(int N, int K, int target, int[] coins) {
+        boolean[][] d = new boolean[target + 1][K + 1];
+        d[0][0] = true;
+           for (int i = 1; i <= target; i++) 
+           {
+            for (int j = 0; j <= K; j++) 
+            {
+               for (int coin : coins) 
+               {
+                    if (coin <= i && j > 0 && d[i - coin][j - 1]) 
+                    {
+                        d[i][j] = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+       return d[target][K];
+    }
+}
